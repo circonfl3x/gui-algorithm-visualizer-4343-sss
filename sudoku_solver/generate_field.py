d3691bdc460c02ef7cc63e60b622ccae639ae9a2
wosukiro <serpontynoff@gmail.com>
2026-07-06 19:13:04 +0300

    generator upd

--- sudoku_solver/generate_field.py
+++ sudoku_solver/generate_field.py
@@ -2,11 +2,14 @@ from dokusan import generators, solvers
 from pprint import pprint
 import numpy as np
 
-field = generators.random_sudoku(avg_rank=100)
-field_np = np.array(list(str(field)), dtype=int).reshape(9, 9)
+def generate_puzzle():
+    field = generators.random_sudoku(avg_rank=100)
+    field_np = np.array(list(str(field)), dtype=int).reshape(9, 9)
 
-solution = solvers.backtrack(field)
-solution_np = np.array(list(str(solution)), dtype=int).reshape(9, 9)
+    return field, field_np
 
-pprint(field_np)
-pprint(solution_np)
+def get_solution(field):
+    solution = solvers.backtrack(field)
+    solution_np = np.array(list(str(solution)), dtype=int).reshape(9, 9)
+
+    return solution, solution_np
