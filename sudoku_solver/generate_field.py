b6ae86fbc80dda508cd589ed1346cda974a93677
Sayalel Owesi <sayalelowesi456@gmail.com>
2026-07-08 04:03:39 +0300

    Добавил считения из файла и терминала. Начинал писать сам алгоритм (пока что только имплементация классов individual и population и рандомное заполнение поля

--- sudoku_solver/generate_field.py
+++ sudoku_solver/generate_field.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 import time
+from os import sys
 
 def generate_puzzle():
     field = generators.random_sudoku(avg_rank=100)
@@ -15,6 +16,54 @@ def get_solution(field):
     solution_np = np.array(list(str(solution)), dtype=int).reshape(9, 9)
 
     return solution, solution_np
+def field_from_input():
+    field = []
+    line_num = 0
+    for line in sys.stdin:
+        line_num += 1
+        if line_num > 9:
+            break
+        line = line.strip()
+        if not line:
+            continue
+        row = [int(x) for x in line.split()]
+        if(len(row) != 9):
+            raise ValueError("Row should be 9 digits long")
+        for i in row:
+            if i < 0 or i>9:
+                raise ValueError(f"Element on position {i+1}, line {line_num+1} is out of range")
+        field.append(row)
+    return field
+def field_from_file(filename):
+    field = []
+    line_num = 0
+
+    try:
+        with open(filename, 'r') as file:
+            
+            for line in file:
+                line_num += 1
+                if line_num > 9:
+                    break
+
+                line = line.strip()
+
+                if not line:
+                    continue
+
+                row = [int(x) for x in line.split()]
+
+                if(len(row) != 9):
+                    raise ValueError("Row should be 9 digits long")
+
+                for i in row:
+                    if i < 0 or i>9:
+                        raise ValueError(f"Element on position {i+1}, line {line_num+1} is out of range")
+                field.append(row)
+    except FileNotFoundError as f:
+        print(f"file {filename} not found")
+    return field
+
 
 def field_to_img(field_np, cell_size=60, line_color = 'black', text_color='black', bg='white'): # 0.114502... - Время выполнения
     width = height = cell_size * 9
